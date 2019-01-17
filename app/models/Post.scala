83f8c570b228ac46521f60d30a3d2ad45536838b
Konstantin Musienko <kmusienko97@gmail.com>
2019-01-17 15:54:14 +0200

    Add scala-docs for the Post entity

--- app/models/Post.scala
+++ app/models/Post.scala
@@ -1,5 +1,12 @@
 package models
 
+/**
+  * The Post entity.
+  *
+  * @param id      an id of the post
+  * @param title   a title of the post
+  * @param content a content of the post
+  */
 case class Post(id: Option[Long] = None,
                 title: String,
                 content: String)
