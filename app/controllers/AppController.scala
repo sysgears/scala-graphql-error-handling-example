e8786ee8dc91177179934fdbf9ca4acf269d20f7
Konstantin Musienko <kmusienko97@gmail.com>
2019-01-17 18:07:44 +0200

    Add scala-docs for AppController

--- app/controllers/AppController.scala
+++ app/controllers/AppController.scala
@@ -18,13 +18,24 @@ import scala.util.{Failure, Success, Try}
 /**
   * This controller creates an `Action` to handle HTTP requests to the
   * application's home page.
+  *
+  * @param controllerComponents a base controller components dependencies that most controllers rely on.
+  * @param graphQL an object that 
   */
 @Singleton
-class AppController @Inject()(cc: ControllerComponents,
-                              graphQL: GraphQL) extends AbstractController(cc) {
+class AppController @Inject()(controllerComponents: ControllerComponents,
+                              graphQL: GraphQL) extends AbstractController(controllerComponents) {
 
+  /**
+    * Renders a page with an in-browser IDE for exploring GraphQL.
+    */
   def graphiql: Action[AnyContent] = Action(Ok(views.html.graphiql()))
 
+  /**
+    * Parses GraphQL body of incoming request and executes GraphQL query.
+    *
+    * @return an 'Action' that handles a request and generates a result to be sent to the client
+    */
   def graphqlBody: Action[JsValue] = Action.async(parse.json) {
     implicit request: Request[JsValue] =>
 
@@ -58,15 +69,32 @@ class AppController @Inject()(cc: ControllerComponents,
       }
   }
 
+  /**
+    * Renders a GraphQL schema.
+    */
   def renderSchema = Action {
     Ok(SchemaRenderer.renderSchema {
       graphQL.Schema
     })
   }
 
+  /**
+    * Parses variables of incoming query.
+    *
+    * @param variables variables from incoming query
+    * @return JsObject with variables
+    */
   def parseVariables(variables: String): JsObject = if (variables.trim.isEmpty || variables.trim == "null") Json.obj()
   else Json.parse(variables).as[JsObject]
 
+  /**
+    * Analyzes and executes incoming GraphQL query and returns the execution result.
+    *
+    * @param query     GraphQL body of request
+    * @param variables incoming variables passed in the request
+    * @param operation a name of the operation (queries or mutations)
+    * @return simple result which defines the response header and the body to send to the client
+    */
   def executeQuery(query: String, variables: Option[JsObject] = None, operation: Option[String] = None): Future[Result] = QueryParser.parse(query) match {
     case Success(queryAst: Document) => Executor.execute(
       schema = graphQL.Schema,
